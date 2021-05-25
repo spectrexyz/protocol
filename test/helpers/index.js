0eabc13a05eb176acdc6e00346531b92ab440ef6
Pierre Bertet <hello@pierre.world>
2021-05-25 10:36:48 +0100

    Tests: fix import case

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -1,6 +1,6 @@
-const SERC20 = require('../../artifacts/contracts/core/SERC20.sol/sERC20.json');
+const SERC20 = require('../../artifacts/contracts/core/SERC20.sol/SERC20.json');
 const SERC721 = require('../../artifacts/contracts/core/SERC721.sol/sERC721.json');
-const SERC1155 = require('../../artifacts/contracts/core/SERC1155.sol/sERC1155.json');
+const SERC1155 = require('../../artifacts/contracts/core/SERC1155.sol/SERC1155.json');
 const CloneFactory = require('../../artifacts/contracts/test/CloneFactory.sol/CloneFactory.json');
 
 module.exports = {
