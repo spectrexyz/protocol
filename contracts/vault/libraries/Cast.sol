c4a532d12fd7adce41319cc5626b23b41001ef18
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2021-08-25 18:06:51 +0200

    Clean Cast.sol library

--- contracts/vault/libraries/Cast.sol
+++ contracts/vault/libraries/Cast.sol
@@ -5,7 +5,7 @@ import "../../token/interfaces/sIERC20.sol";
 
 /**
  * @title Cast
- * @dev - Enables casting between sERC1155 token types ids and sERC20 addresses.
+ * @dev - Enables casting between ERC1155 token types ids and sERC20 addresses.
  *      - Enables casting from bytes32 to string.
  */
 library Cast {
@@ -22,14 +22,16 @@ library Cast {
     }
 
     function toString(bytes32 _bytes32) internal pure returns (string memory) {
-        uint8 i = 0;
+        uint256 i = 0;
         while (i < 32 && _bytes32[i] != 0) {
             i++;
         }
-        bytes memory bytesArray = new bytes(i);
+        
+        bytes memory characters = new bytes(i);
         for (i = 0; i < 32 && _bytes32[i] != 0; i++) {
-            bytesArray[i] = _bytes32[i];
+            characters[i] = _bytes32[i];
         }
-        return string(bytesArray);
+
+        return string(characters);
     }
 }
