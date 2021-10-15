3a1d23ecbd3ca49cc88895d28b0e418394b17ad5
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2021-10-15 12:01:38 +0200

    Update deployment scripts

--- deploy/8_channeler.js
+++ deploy/8_channeler.js
@@ -3,6 +3,7 @@ const func = async (hre) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
+  const sERC721 = await ethers.getContract("sERC721");
   const vault = await ethers.getContract("Vault");
   const issuer = await ethers.getContract("Issuer");
   const broker = await ethers.getContract("Broker");
@@ -10,7 +11,7 @@ const func = async (hre) => {
 
   await deploy("Channeler", {
     from: deployer,
-    args: [vault.address, issuer.address, broker.address, splitter.address],
+    args: [sERC721.address, vault.address, issuer.address, broker.address, splitter.address],
     log: true,
   });
 };
