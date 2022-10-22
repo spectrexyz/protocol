de315f5b88124f801f97a7b33f15610b93008d36
Pierre Bertet <hello@pierre.world>
2022-10-22 01:00:39 +0100

    Fractionalize: buy from multiple accounts
    Also:
    
    - Move config + fractionalize script to TS
    - Move accounts and RPC URLs to env vars
    - Use the main config account as guardian
    - Add dprint

---
+++ hardhat.config.ts
@@ -0,0 +1,76 @@
+import type { HardhatUserConfig } from "hardhat/config";
+
+import "@nomiclabs/hardhat-waffle";
+import "hardhat-deploy";
+import "hardhat-deploy-ethers";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const testnetAccounts = [
+  process.env.ACCOUNT_PK1,
+  process.env.ACCOUNT_PK2,
+  process.env.ACCOUNT_PK3,
+].filter(Boolean) as string[];
+
+if (testnetAccounts.length === 0) {
+  throw new Error("The env vars ACCOUNT_PK{1,2,3} are missing.");
+}
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 2000,
+          },
+        },
+      },
+      {
+        version: "0.7.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        count: 50,
+        accountsBalance: "1000000000000000000000000",
+      },
+      allowUnlimitedContractSize: true,
+    },
+    rinkeby: {
+      accounts: testnetAccounts,
+      allowUnlimitedContractSize: true,
+      url: process.env.RPC_RINKEBY,
+    },
+    goerli: {
+      accounts: testnetAccounts,
+      allowUnlimitedContractSize: true,
+      gas: 21000000,
+      url: process.env.RPC_GOERLI,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+    },
+  },
+  gasReporter: {
+    enabled: Boolean(process.env.REPORT_GAS),
+  },
+};
+
+export default config;
---
+++ scripts/fractionalize.ts
@@ -0,0 +1,153 @@
+import type { BigNumber } from "ethers";
+
+import chalk from "chalk";
+import dotenv from "dotenv";
+import { deployments, ethers, network } from "hardhat";
+
+dotenv.config();
+
+type Signer = Awaited<ReturnType<typeof ethers.getSigners>>[number];
+
+async function getContract(name: string) {
+  const contract = await deployments.get(name);
+  return ethers.getContractAt(contract.abi, contract.address);
+}
+
+const terminal = {
+  print: (msg: string) => {
+    process.stdout.write(msg);
+  },
+  info: (msg: string) => {
+    console.log(msg);
+  },
+  newline: () => {
+    console.log("");
+  },
+  success: (msg: string) => {
+    console.log(chalk.green(msg));
+  },
+  title: (msg: string) => {
+    console.log(chalk.cyan(`-]] ${msg.toUpperCase()} [[-`));
+    console.log(chalk.cyan(`--------------`));
+  },
+  subtitle: (msg: string) => {
+    console.log(chalk.yellow(msg));
+  },
+  error: (msg: string) => {
+    console.log(chalk.red(msg));
+  },
+};
+
+const data = (guardian: string) => ({
+  guardian,
+  name: "NFT Fraction",
+  symbol: "FRAC",
+  cap: ethers.utils.parseEther("1000000"),
+  buyoutReserve: ethers.utils.parseEther("100"),
+  multiplier: ethers.utils.parseEther("1.5"),
+  timelock: ethers.BigNumber.from("864000"), // 10 days
+  sMaxNormalizedWeight: ethers.BigNumber.from("800000000000000000"),
+  sMinNormalizedWeight: ethers.BigNumber.from("500000000000000000"),
+  beneficiaries: [guardian],
+  shares: [ethers.utils.parseEther("15")],
+  swapFeePercentage: ethers.BigNumber.from("10000000000000000"),
+  issuanceReserve: ethers.utils.parseEther("100"),
+  fee: ethers.utils.parseEther("5"),
+  buyoutFlash: true,
+  issuanceFlash: true,
+});
+
+const approve = async () => {
+  const sERC721 = await getContract("sERC721");
+  const vault = await getContract("Vault");
+
+  terminal.print("» Approving vault to transfer NFTs… ");
+  const tx = await sERC721.setApprovalForAll(vault.address, true);
+  await tx.wait();
+  terminal.success("OK");
+};
+
+const fractionalize = async () => {
+  const sERC721 = await getContract("sERC721");
+  const channeler = await getContract("Channeler");
+  const vault = await getContract("Vault");
+
+  if (!process.env.TOKEN_URI) {
+    throw new Error("The TOKEN_URI environment variable is missing.");
+  }
+
+  terminal.print(
+    `» Fractionalizing NFT with URI ${process.env.TOKEN_URI}… `,
+  );
+
+  const [account] = await ethers.getSigners();
+  const tx = await channeler.mintAndFractionalize(
+    process.env.TOKEN_URI,
+    data(await account.getAddress()),
+  );
+  await tx.wait();
+  terminal.success("OK");
+
+  const events1 = (await sERC721.queryFilter(sERC721.filters.Transfer()))
+    .filter((event) => event.event === "Transfer");
+  const tokenId = events1[events1.length - 1].args?.tokenId;
+
+  const events2 = (await vault.queryFilter(vault.filters.Fractionalize()))
+    .filter((event) => event.event === "Fractionalize");
+  const id = events2[events2.length - 1].args?.id;
+  const sERC20Address = await vault.sERC20Of(id);
+
+  terminal.info(
+    `»» ERC721: https://testnets.opensea.io/assets/${sERC721.address}/${tokenId}`,
+  );
+  terminal.info(
+    `»» ERC1155: https://testnets.opensea.io/assets/${vault.address}/${id}`,
+  );
+  terminal.info(
+    `»» sERC20: https://${network.name}.etherscan.io/token/${sERC20Address}`,
+  );
+
+  return sERC20Address;
+};
+
+const issue = async (
+  sERC20: string,
+  from: Signer,
+  amount: BigNumber,
+) => {
+  const expected = ethers.BigNumber.from("0");
+  const issuer = await getContract("Issuer");
+
+  terminal.print(
+    `» Buying ${
+      ethers.utils.formatEther(amount)
+    } ETH worth of sERC20 from ${await from.getAddress()}… `,
+  );
+
+  const tx = await issuer.connect(from).issue(sERC20, expected, {
+    value: amount,
+  });
+  await tx.wait();
+
+  terminal.success("OK");
+};
+
+const main = async () => {
+  await approve();
+  const sERC20 = await fractionalize();
+
+  for (const [index, signer] of (await ethers.getSigners()).entries()) {
+    await issue(
+      sERC20,
+      signer,
+      ethers.utils.parseEther(["0.01", "0.02", "0.03"][index % 3]),
+    );
+  }
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    terminal.error(error.message);
+    process.exit(1);
+  });
